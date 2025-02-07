b89409c9b90afac1b51bdf53876784828cb55715
Lieven De Troch <ldetroch@outlook.com>
2025-02-07 21:16:04 +0100

    Refactor image tag handling in determine-build-target.sh to use JSON format and update Docker workflow to utilize new IMAGE_TAGS variable

--- .github/scripts/determine-build-target.sh
+++ .github/scripts/determine-build-target.sh
@@ -1,26 +1,20 @@
 #!/bin/bash
 
-IMAGE_TAG=()
-if [[ "${GITHUB_REF}" == refs/tags/* ]]; then
-  # This is a tag event; extract the tag name
-  TAG_NAME="${GITHUB_REF#refs/tags/}"
-  IMAGE_TAG=("type=raw,value=${TAG_NAME},enable=true")
-  echo "Tag event detected. Using image tag: ${IMAGE_TAG[@]}"
-else
+IMAGE_TAGS=()
   # Extract branch name from GITHUB_REF (e.g. refs/heads/dev becomes dev)
   BRANCH_NAME="${GITHUB_REF#refs/heads/}"
   if [ "$BRANCH_NAME" == "dev" ]; then
     DATE_TAG=$(date +%Y-%m-%d-%H-%M)
-    IMAGE_TAG=("type=raw,value=dev-${DATE_TAG},enable=true" "type=raw,value=dev,enable=true")
-    echo "Dev branch detected. Using image tag: ${IMAGE_TAG[@]}"
+    IMAGE_TAGS='["type=raw,value=dev-'${DATE_TAG}',enable=true", "type=raw,value=dev,enable=true"]'
+    echo "Dev branch detected. Using image tag: ${IMAGE_TAGS}"
 
   elif [ "$BRANCH_NAME" == "release" ]; then
     # Ensure that a tag is present
     if git diff-tree --no-commit-id --name-only -r HEAD | grep -q '^VERSION'; then
       echo "VERSION file was updated in this commit."
       VERSION=$(cat VERSION)
-      IMAGE_TAG=("type=raw,value=${VERSION}-rc,enable=true" "type=raw,value=latest-rc,enable=true")
-      echo "RC branch detected. Using image tag: ${IMAGE_TAG[@]}"
+      IMAGE_TAGS='["type=raw,value='${VERSION}'-rc,enable=true", "type=raw,value=latest-rc,enable=true"]'
+      echo "RC branch detected. Using image tag: ${IMAGE_TAGS}"
     else
       echo "ERROR: VERSION file not updated on RC branch or no tag present. Aborting build."
       exit 1
@@ -31,8 +25,8 @@ else
     if git diff-tree --no-commit-id --name-only -r HEAD | grep -q '^VERSION'; then
       echo "VERSION file was updated in this commit."
       VERSION=$(cat VERSION)
-      IMAGE_TAG=("type=raw,value=${VERSION},enable=true" "type=raw,value=latest,enable=true")
-      echo "Release branch detected. Using image tag: ${IMAGE_TAG[@]}"
+      IMAGE_TAGS='["type=raw,value='${VERSION}',enable=true", "type=raw,value=latest,enable=true"]'
+      echo "Release branch detected. Using image tag: ${IMAGE_TAGS}"
     else
       echo "ERROR: VERSION file not updated on release branch or no tag present. Aborting build."
       exit 1
@@ -41,6 +35,6 @@ else
     echo "Branch ${BRANCH_NAME} does not have a defined build strategy. Aborting."
     exit 1
   fi
-fi
-# Export the IMAGE_TAG for later steps
-echo "IMAGE_TAG=${IMAGE_TAG[@]}" >> $GITHUB_OUTPUT
+
+# Export the IMAGE_TAGS for later steps
+echo "IMAGE_TAGS=${IMAGE_TAGS}" >> $GITHUB_OUTPUT
