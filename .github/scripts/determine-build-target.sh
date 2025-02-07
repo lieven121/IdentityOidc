171de357ab73f6f84a4599d4462b1078d1c2312d
Lieven De Troch <ldetroch@outlook.com>
2025-02-07 20:31:22 +0100

    Refactor image tag generation in build script and update Docker workflow to use dynamic tags

--- .github/scripts/determine-build-target.sh
+++ .github/scripts/determine-build-target.sh
@@ -11,7 +11,8 @@ else
   BRANCH_NAME="${GITHUB_REF#refs/heads/}"
   if [ "$BRANCH_NAME" == "dev" ]; then
     DATE_TAG=$(date +%Y-%m-%d-%H-%M)
-    IMAGE_TAG="type=raw,value=dev-${DATE_TAG},enable=true/ntype=raw,value=dev,enable=true"
+    IMAGE_TAG="type=raw,value=dev-${DATE_TAG},enable=true 
+    type=raw,value=dev,enable=true"
     echo "Dev branch detected. Using image tag: ${IMAGE_TAG}"
 
   elif [ "$BRANCH_NAME" == "release" ]; then
@@ -19,19 +20,21 @@ else
     if git diff-tree --no-commit-id --name-only -r HEAD | grep -q '^VERSION'; then
       echo "VERSION file was updated in this commit."
       VERSION=$(cat VERSION)
-      IMAGE_TAG="type=raw,value=rc-${VERSION},enable=true/ntype=raw,value=latest-rc,enable=true"
+      IMAGE_TAG="type=raw,value=rc-${VERSION},enable=true
+      type=raw,value=latest-rc,enable=true"
       echo "RC branch detected. Using image tag: ${IMAGE_TAG}"
     else
       echo "ERROR: VERSION file not updated on RC branch or no tag present. Aborting build."
       exit 1
     fi
-    
+
   elif [ "$BRANCH_NAME" == "master" ]; then
     # Ensure that the VERSION file was updated and a tag is present
     if git diff-tree --no-commit-id --name-only -r HEAD | grep -q '^VERSION'; then
       echo "VERSION file was updated in this commit."
       VERSION=$(cat VERSION)
-      IMAGE_TAG="type=raw,value=${VERSION},enable=true/ntype=raw,value=latest,enable=true"
+      IMAGE_TAG="type=raw,value=${VERSION},enable=true
+      type=raw,value=latest,enable=true"
       echo "Release branch detected. Using image tag: ${IMAGE_TAG}"
     else
       echo "ERROR: VERSION file not updated on release branch or no tag present. Aborting build."
