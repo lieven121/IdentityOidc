fd9db125a4fca635285a8eb18546b74b942160e8
Lieven De Troch <ldetroch@outlook.com>
2025-02-08 00:35:16 +0100

    Add https workaround until i understand proxies better

--- Identity.App/Hosting/HostingConfig.cs
+++ Identity.App/Hosting/HostingConfig.cs
@@ -1,4 +1,6 @@
-﻿namespace Identity.App.Hosting;
+﻿using Microsoft.AspNetCore.HttpOverrides;
+
+namespace Identity.App.Hosting;
 public static class HostingConfig
 {
     public static IApplicationBuilder UseUrlsFromConfig(this WebApplication app)
@@ -16,6 +18,39 @@ public static class HostingConfig
                 app.Urls.Add(url);
             }
         }
+        return app;
+    }
+
+
+    public static IHostApplicationBuilder ConfigureReverseProxySupport(this IHostApplicationBuilder app)
+    {
+
+        var rProxySupport = app.Configuration.GetSection("Hosting:ReverseProxySupport")?.Get<bool>() != false;
+        if(rProxySupport)
+            app.Services.Configure<ForwardedHeadersOptions>(options =>
+            {
+                options.ForwardedHeaders = ForwardedHeaders.XForwardedProto;
+            });
+
+        return app;
+    }
+
+    public static WebApplication UseReverseProxySupport(this WebApplication app)
+    {
+        var rProxySupport = app.Configuration.GetSection("Hosting:ReverseProxySupport")?.Get<bool>() != false;
+
+        if(rProxySupport)
+            app.UseCertificateForwarding();
+
+        var assumeEveryRequestHttps = app.Configuration.GetSection("Hosting:AssumeEveryRequestHttps")?.Get<bool>() == true;
+        if (assumeEveryRequestHttps)
+            app.Use((context, next) =>
+            {
+                context.Request.Scheme = "https";
+
+                return next();
+            });
+
         return app;
     }
 
