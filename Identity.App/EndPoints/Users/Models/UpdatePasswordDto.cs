b2bb6fc5815fa847481ff56155a894c9e282782b
Lieven De Troch <ldetroch@outlook.com>
2025-08-24 19:37:24 +0200

    Add Internal endpoints

---
+++ Identity.App/EndPoints/Users/Models/UpdatePasswordDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Identity.App.EndPoints.Users.Models;
+
+public class UpdatePasswordDto
+{
+    public string CurrentPassword { get; set; } = string.Empty;
+    public string NewPassword { get; set; } = string.Empty;
+
+}
