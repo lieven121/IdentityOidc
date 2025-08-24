349003a6bf3a34fba2712648d2fc3a16281916ff
Lieven De Troch <ldetroch@outlook.com>
2025-08-24 19:32:52 +0200

    Add External endpoints

---
+++ Identity.App/EndPoints/External/Models/CreateUserDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Identity.App.EndPoints.External.Models;
+
+public class CreateUserDto
+{
+    public string Username { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+
+    public bool EmailConfirmed { get; set; } = true;
+}
---
+++ Identity.App/EndPoints/External/Models/UsersFilter.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Identity.App.EndPoints.External.Models;
+
+public class UsersFilter
+{
+    [FromQuery]
+    public string Email { get; set; } = string.Empty;
+    [FromQuery]
+    public string Username { get; set; } = string.Empty;
+
+    [FromQuery]
+    public int Page { get; set; } = 0;
+    [FromQuery]
+    public int PageSize { get; set; } = 10;
+    //public string SortBy { get; set; } = "Email";
+    //public bool SortDescending { get; set; } = false;
+}
