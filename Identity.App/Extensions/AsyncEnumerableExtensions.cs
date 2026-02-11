49d7516de48d01234d76a78d39514cfd29c11419
lieven121 <ldetroch@outlook.com>
2026-02-11 23:02:47 +0100

    .net 10

--- Identity.App/Extensions/AsyncEnumerableExtensions.cs
+++ Identity.App/Extensions/AsyncEnumerableExtensions.cs
@@ -3,13 +3,13 @@
 //https://stackoverflow.com/questions/59380470/convert-iasyncenumerable-to-list
 public static class AsyncEnumerableExtensions
 {
-    public static async Task<List<T>> ToListAsync<T>(this IAsyncEnumerable<T> items,
-        CancellationToken cancellationToken = default)
-    {
-        var results = new List<T>();
-        await foreach (var item in items.WithCancellation(cancellationToken)
-                                        .ConfigureAwait(false))
-            results.Add(item);
-        return results;
-    }
+    //public static async Task<List<T>> ToListAsync<T>(this IAsyncEnumerable<T> items,
+    //    CancellationToken cancellationToken = default)
+    //{
+    //    var results = new List<T>();
+    //    await foreach (var item in items.WithCancellation(cancellationToken)
+    //                                    .ConfigureAwait(false))
+    //        results.Add(item);
+    //    return results;
+    //}
 }
